63323d5a471cd06fdf87fe6685e7841067a66758
Marley Stapff <kirkstapff88@gmail.com>
2023-04-06 00:33:32 -0300

    add some oauth

--- App.js
+++ App.js
@@ -1,14 +1,36 @@
 import { StatusBar } from 'expo-status-bar';
 import { Button, StyleSheet, Text, View } from 'react-native';
-import * as WebBrowser from 'expo-web-browser'
+import * as AuthSession from 'expo-auth-session';
 
-const client_key = ""
+const client_key = "aw3oc4nve4o8l67h"
+const tiktok_scope = "user.info.basic"
 
 export default function App() {
+
+  const handleTiktokSignIn = async () => {
+    const redirectUrl = AuthSession.getRedirectUrl();
+    const result = await AuthSession.startAsync({
+      authUrl:
+        `https://www.tiktok.com/auth/authorize?` +
+        `&response_type=code` +
+        `&client_key=${client_key}` +
+        `&redirect_uri=${encodeURIComponent(redirectUrl)}` +
+        `&scope=${tiktok_scope}` +
+        `&state=abc123`
+    });
+
+    if (result.type === 'success') {
+      console.log(result)
+      return
+    }
+    console.log("failed")
+    console.log(result)
+  };
+
   return (
     <View style={styles.container}>
       <Text>Connect your accounts</Text>
-      <Button title="A" onPress={()=>{WebBrowser.openAuthSessionAsync(`https://www.tiktok.com/auth/authorize?client_key=${client_key}&response_type=code&scope`)}}/>
+      <Button title="A" onPress={handleTiktokSignIn}/>
       <StatusBar style="auto" />
     </View>
   );
